6311bca5b01bf0d1375d3267f58ed6034823a077
Luci <54627795+megafabr@users.noreply.github.com>
2019-11-09 10:18:19 +0300

    Update cart.rb

--- lib/cart.rb
+++ lib/cart.rb
@@ -1,32 +1,31 @@
 class Cart
 def initialize
   @products = []
-  # @cart_price = 0
 end
 
   def next_choice_to_cart(chosen_product)
-    @chosen_product = chosen_product
     @purchase_price = chosen_product.price
     @products << chosen_product
-    # @cart_price += @purchase_price
+  end
+
+  def total_sum
+    @products.sum(&:price)
   end
 
   def to_s
-    cart_price = @products.sum { |product| product.price.to_i }
-    page = <<-HTML
-    С Вас - #{cart_price} руб. Спасибо за покупку!
+    <<-HTML
+    С Вас - #{total_sum} руб. Спасибо за покупку!
     Вы выбрали: #{@chosen_product}
 
     HTML
   end
 
   def print_cart
-    cart_price = @products.sum { |product| product.price.to_i }
-    page = <<-HTML
+    <<-HTML
     Вы купили: 
     #{@products.join("\n    ")}
 
-    С Вас - #{cart_price} руб. Спасибо за покупки!
+    С Вас - #{total_sum} руб. Спасибо за покупки!
     HTML
   end
 end
